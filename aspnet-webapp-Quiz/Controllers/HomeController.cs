57584f484cac2b9c24e59fa7ee43f4d223b23d54
MuhammadSohail <msohai11@my.centennialcollege.ca>
2024-03-25 14:31:05 -0400

    Database added

--- aspnet-webapp-Quiz/Controllers/HomeController.cs
+++ aspnet-webapp-Quiz/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace aspnet_webapp_Quiz.Controllers
 
         public IActionResult Index()
         {
-            // Add the message here
+            /
             ViewBag.Message = "Database access has already been done on April 14, 2023 by Muhammad Farhan Sohail";
 
             return View();
